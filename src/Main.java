675f3d5a4d879e351154771e4d71883624187fb8
666hellmaster <annonymause@email.cz>
2023-07-31 09:36:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int s;
+
+
+        System.out.println("zadejte hodnotu stran čtverce: ");
+        s = scanner.nextInt();
+
+        for (int j = 0; j < s; j++){
+
+            for (int i = 0; i < s; i++) {
+
+                    if (j == 0 || j == s - 1 || i == 0 || i == s - 1 ){
+                    System.out.printf("*" + " ");
+
+                    }else{
+                        System.out.printf(" " + " ");
+                             }
+            }
+            System.out.println();
+        }
+    }
+}
